ea6186d5ecf89aa0678cd7b5a50282356716fb96
aymane-adam <144238779+aymane-adam@users.noreply.github.com>
2024-05-14 16:18:43 +0200

    voile de con

---
+++ php/nav.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Naveil</title>
+
+  <link rel="stylesheet" href="../css/navfooter.css">
+</head>
+
+
+<body>
+
+    <header>
+        <nav id="navop">
+
+        <div class="nav-veil">
+                <img src="../img/neil-nav.png" alt="veil">
+        </div>
+       </nav>
+
+    </header>
+  
+
+
+</body>
