e993a5ca172e6eb655da795867db1da38f555bb5
matheo-lesage-gante <matheo.lesage-gante@student.junia.com>
2024-05-14 16:02:20 +0200

    regle

---
+++ php/regles.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/regles.css">
+        <title>Lost Island</title>
+    </head>
+    <body>
+        <div>
+            <img src="../img/regles.png" alt="Photo d'un parchemin"/>
+            <h1>LES REGLES</h1>
+            <ul>
+                <li>La but du jeu est d'arrivé à la case arrivé</li>
+                <li>Placer tout les bloques avant de lancer le niveau</li>
+                <li>Pour chaque obstacle toucher cela entraine la fin du niveau</li>
+                <li>Le nombre de bloque à poser est limité</li>
+                <li>Le temps pour résoudre le niveau est illimités</li>
+                <li>Le nombre de point se fera de la maniere suivante : </li>
+            </ul>
+        </div> 
+    </body>
+</html>
