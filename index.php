1cdda4325f44d76e60b818cacdeff33a437a32cf
Calliste-Lefebvre <calliste.lefebvre@student.junia.com>
2024-05-14 16:05:32 +0200

    jour 2

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 </head>
 <body>
     <h1>Lost Island</h1>
+    <div class="container">
+        <img src="img/planches.webp" alt="Image" style="width:30%">
+        <div class="text">
+            <h2>Play</h2>
+        </div>
+    </div>
 </body>
 </html>
