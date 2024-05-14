06c7fc61612af941a68dc72dbbe9c299bc353eb9
AllienneAdrien <144245192+AllienneAdrien@users.noreply.github.com>
2024-05-14 14:47:15 +0200

    KK

--- php/creation.php
+++ php/creation.php
@@ -8,6 +8,6 @@
 </head>
 <body>
     <h1>Création d'un niveau</h1>
-    <img class="gif" src="../img/akrien.gif" width="50%" height="100%" >
+    <img class="gif" src="../img/picpirate.gif" width="50%" height="100%" >
 </body>
 </html>
