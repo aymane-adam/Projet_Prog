3be4235e0310cb2f868b34935e16ab6a4e17f5f0
MartelFlavien <flavien.martel@student.junia.com>
2024-05-16 14:29:51 +0200

    jtm
    jtm

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>Lost Island</h1>
-    <a href="game.html" class="container">
+    <a href="php/game.html" class="container">
         <img src="img/planches.webp" alt="Image" style="width:30%">
         <div class="text">
             <h2>Play</h2>
---
+++ php/game.php
@@ -0,0 +1,40 @@
+<!Doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Main</title>
+        <link rel="stylesheet" href="css/test.css">
+    </head>
+    <body>
+        <div id="bateau">
+            <div class="voile">
+                <div class="tete">
+                    <figure id="voile1">
+                        <img src="img/voilenav.png">
+                    </figure>
+                    <figure id="voile2" class="image-container">
+                        <img src="img/voilePleine.png">
+                        <figcaption class="overlay-text">
+                            <a href="game.html">Mode <br>communautaire</a>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="tete2">
+                    <figure id="voile3">
+                        <img src="img/voilenav.png">
+                    </figure>
+                    <figure id="voile4" class="image-container">
+                        <img src="img/voilePleine.png">
+                        <figcaption class="overlay-text">
+                            <a href="game.html">Mode <br>campagne</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div id="coque">
+                <img src="img/coque.gif">
+            </div>
+        </div>
+    </body>
+</html>
