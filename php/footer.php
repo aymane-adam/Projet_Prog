0e2718743ab2e166666b11b57a3149d10dac5b18
aymane-adam <144238779+aymane-adam@users.noreply.github.com>
2024-05-16 14:10:11 +0200

    FOOTER NAV

---
+++ php/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>footVEIL</title>
+<link rel="stylesheet" href="../css/navfooter.css">
+</head>
+
+<body>
+<footer id="footer-yu">
+<div class="coque">
+                <img src="../img/coque_pirate.png" alt="veil">
+        </div>
+
+</footer>
+
+</body>
