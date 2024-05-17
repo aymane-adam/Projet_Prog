aecf1cb5cd7b64aeb802d061c78bdd49e6a31f51
matheo-lesage-gante <matheo.lesage-gante@student.junia.com>
2024-05-17 11:09:12 +0200

    nav

--- php/nav.php
+++ php/nav.php
@@ -25,10 +25,14 @@ function toggleImages() {
   if (veil1.style.opacity === '0' || veil1.style.opacity === '') {
     veil1.style.opacity = '1'; // Faire apparaître la première image
     veil2.style.display = 'none'; // Cacher la deuxième image
+    veil1.style.zIndex = '9';
+    veil2.style.zIndex = '10';
   } else {
     veil1.style.opacity = '0'; // Faire disparaître la première image
     veil2.style.opacity = '1';
     veil2.style.display = 'block'; // Afficher la deuxième image
+    veil1.style.zIndex = '10';
+    veil2.style.zIndex = '9';
   }
 }
 
