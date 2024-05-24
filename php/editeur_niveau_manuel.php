86e7b86b8ca8ef0645488b3f1b92d705e532af2b
matheo-lesage-gante <matheo.lesage-gante@student.junia.com>
2024-05-24 13:33:01 +0200

    editeur

--- php/editeur_niveau_manuel.php
+++ php/editeur_niveau_manuel.php
@@ -18,23 +18,56 @@
             for ($i = 0; $i < $lignes; $i++) {
                 echo "<tr>";
                 for ($j = 0; $j < $colonnes; $j++) {
-                    echo "<td></td>";
+                    echo '<td><div class="drop-box" style="height: 100%; width: 100%;"></div></td>';
                 }
                 echo "</tr>";
             }
             echo "</table>";
         }
+
         if(array_key_exists('button1', $_POST)){
-            $lignes = intval($_POST['taille']);
-            Grille($lignes, $lignes);
+            $tailles = intval($_POST['taille']);
+            Grille($tailles, $tailles);
         }
     ?>
-    <button type="submit" class="image-button">
-        <img src="../pixel_art_projet/32x32/bateau.png" alt="Bateau">
-    </button>
+
+    <div class="image-container">
+        <img src="../pixel_art_projet/32x32/bateau.png" draggable="true" id="image1" alt="Boat">
+        <img src="../pixel_art_projet/32x32/guardianclose.png" draggable="true" id="image2" alt="Guardian">
+        <img src="../pixel_art_projet/32x32/krabby.png" draggable="true" id="image3" alt="Krabby">
+        <img src="../pixel_art_projet/32x32/pieuvre.png" draggable="true" id="image4" alt="Krakken">
+        <img src="../pixel_art_projet/32x32/rock.png" draggable="true" id="image5" alt="Rock">
+        <img src="../pixel_art_projet/32x32/rock1.png" draggable="true" id="image6" alt="Rock1">
+        <img src="../pixel_art_projet/32x32/vague.png" draggable="true" id="image7" alt="Vague">
+        <img src="../pixel_art_projet/32x32/la_main.png" draggable="true" id="image8" alt="hand">
+    </div>
 
     <script>
-        //drag and drops
+        document.addEventListener("DOMContentLoaded", function() {
+            const images = document.querySelectorAll(".image-container img");
+
+            images.forEach(image => {
+                image.addEventListener("dragstart", function(event) {
+                    event.dataTransfer.setData("text/plain", event.target.id);
+                });
+            });
+
+            const dropBoxes = document.querySelectorAll(".drop-box");
+
+            dropBoxes.forEach(box => {
+                box.addEventListener("dragover", function(event) {
+                    event.preventDefault();
+                });
+
+                box.addEventListener("drop", function(event) {
+                    event.preventDefault();
+                    const imageId = event.dataTransfer.getData("text/plain");
+                    const draggableElement = document.getElementById(imageId).cloneNode(true);
+                    event.target.innerHTML = ''; // Clear any existing content
+                    event.target.appendChild(draggableElement);
+                });
+            });
+        });
     </script>
 </body>
 </html>
