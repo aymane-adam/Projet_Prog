99f79db634ea8ef96d7d3d2f00e1ccac623d9fda
Calliste-Lefebvre <calliste.lefebvre@student.junia.com>
2024-05-27 11:10:48 +0200

    wiki et corbeille

--- php/regles.php
+++ php/regles.php
@@ -11,13 +11,15 @@
         <img src="../img/logolost islland-Photoroom.png" alt="Logo" class="logo">
         
         <div class="container">
+    <div class="image-container">
         <img src="../img/reglement2.png" alt="Règle" class="regle">
         <div class="texte">
             Welcome sailor !<br>
             Le but du jeu est d'atteindre le coffre dans chaque niveau.
-            
         </div>
     </div>
+    <img src="../img/pero.png" alt="Perroquet" class="pero">
+</div>
     <!-- <img src="../sprites/hd/shiphd.png" alt="bateau">
     <img src="../sprites/hd/chest.png" alt="coffre"> -->
         <br>
@@ -25,43 +27,43 @@
     </header>
     <main>
         <div class="obstacle">
-            <img src="../sprites/32x32/ship.png" alt="Le bateau">
+            <img src="../pixel_art_projet/32x32/ship.png" alt="Le bateau">
             <p>Le bateau : C'est lui que tu diriges à l'aide des tridents, sa direction initiale est le nord.</p>
         </div>
         
         <div class="obstacle">
-            <img src="../sprites/32x32/trident2_n.png"" alt="Les tridents">
-            <img src="../sprites/32x32/trident2_e.png"" alt="Les tridents">
-            <img src="../sprites/32x32/trident2_s.png"" alt="Les tridents">
-            <img src="../sprites/32x32/trident2_w.png"" alt="Les tridents">
+            <img src="../pixel_art_projet/32x32/nord.png"" alt="Les tridents">
+            <img src="../pixel_art_projet/32x32/est.png"" alt="Les tridents">
+            <img src="../pixel_art_projet/32x32/sud.png"" alt="Les tridents">
+            <img src="../pixel_art_projet/32x32/ouest.png"" alt="Les tridents">
             <p>Les tridents : ce sont des "flèches de directions", tu en as à chaque début de niveau dans ton inventaire, ce dernier se situe à gauche de l'écran, tu dois les placer avant de lever l'ancre du bateau.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/krabby.png"" alt="Krabby">
+            <img src="../pixel_art_projet/32x32/krabby.png"" alt="Krabby">
             <p>Krabby : c'est un crabe, on ne sait pas d'où cette espèce vient, la légende raconte qu'il vient en réalité d'un autre univers où il existe plein de différentes créatures comme lui avec différentes capacités. Si tu croises son chemin, il va réussir avec la capacité de ses crochets de couper un trident.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/pieuvre.png"" alt="Pieuvre crocheteuse">
+            <img src="../pixel_art_projet/32x32/pieuvre.png"" alt="Pieuvre crocheteuse">
             <p>Pieuvre crocheteuse : Description de la pieuvre crocheteuse.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/la_main.png"" alt="La main de l'eau">
+            <img src="../pixel_art_projet/32x32/la_main.png"" alt="La main de l'eau">
             <p>La main de l'eau : Description de la main de l'eau.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/guardian1.png"" alt="Guardian">
+            <img src="../pixel_art_projet/32x32/guardian1.png"" alt="Guardian">
             <p>Guardian : Description du Guardian.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/vague.png"" alt="Vague">
+            <img src="../pixel_art_projet/32x32/vague.png"" alt="Vague">
             <p>Vague : Description de la vague.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/rock1.png"" alt="Rocher">
+            <img src="../pixel_art_projet/32x32/rock1.png"" alt="Rocher">
             <p>Rocher : si tu passes dessus t'exploses en gros.</p>
         </div>
         <div class="obstacle">
-            <img src="../sprites/32x32/rock.png"" alt="Rocher1">
+            <img src="../pixel_art_projet/32x32/rock.png"" alt="Rocher1">
             <p>Rocher : si tu passes dessus t'exploses en gros.</p>
         </div>
     </main>
