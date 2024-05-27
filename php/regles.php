e8cfbf91025837fdd85a36751e296cfd159c875f
Calliste-Lefebvre <calliste.lefebvre@student.junia.com>
2024-05-27 10:39:54 +0200

    krabby

--- php/regles.php
+++ php/regles.php
@@ -1,27 +1,69 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/regles.css">
-        <title>Lost Island</title>
-    </head>
-    <body>
-        <div>
-            <img src="../img/parchemin.png" alt="parchemin"/>
-            <h1>THE CIR CODE</h1>
-            <dl>
-                <dt>La but du jeu est d'arrivé à la case arrivé</dt>
-                <dt>Placer tout les bloques avant de lancer le niveau</dt>
-                <dt>Pour chaque obstacle toucher cela entraine la fin du niveau</dt>
-                <dt>Le nombre de bloque à poser est dtmité</dt>
-                <dt>Le temps pour résoudre le niveau est ildtmités</dt>
-                <dt>Le nombre de point se fera de la maniere suivante : </dt>
-            </dl>
-            <img id="capitaine" src="../img/capitaine.gif" alt="capitaine"/>
-        </div> 
-        <a id="retour" href="../index.php">
-                <img id="img_retour" src="../img/bouton_retour.png" alt="Image" style="width:13%">
-        </a>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lost Island Wiki</title>
+    <link rel="stylesheet" href="../css/regles.css">
+</head>
+<body>
+    <header>
+        <img src="../img/logolost islland-Photoroom.png" alt="Logo" class="logo">
+        
+        <div class="container">
+        <img src="../img/reglement2.png" alt="Règle" class="regle">
+        <div class="texte">
+            Welcome sailor !<br>
+            Le but du jeu est d'atteindre le coffre dans chaque niveau.
+            
+        </div>
+    </div>
+    <!-- <img src="../sprites/hd/shiphd.png" alt="bateau">
+    <img src="../sprites/hd/chest.png" alt="coffre"> -->
+        <br>
+        <img src="../img/wiki_pancarte.png" alt="Wiki" class="wiki-image">
+    </header>
+    <main>
+        <div class="obstacle">
+            <img src="../sprites/32x32/ship.png" alt="Le bateau">
+            <p>Le bateau : C'est lui que tu diriges à l'aide des tridents, sa direction initiale est le nord.</p>
+        </div>
+        
+        <div class="obstacle">
+            <img src="../sprites/32x32/trident2_n.png"" alt="Les tridents">
+            <img src="../sprites/32x32/trident2_e.png"" alt="Les tridents">
+            <img src="../sprites/32x32/trident2_s.png"" alt="Les tridents">
+            <img src="../sprites/32x32/trident2_w.png"" alt="Les tridents">
+            <p>Les tridents : ce sont des "flèches de directions", tu en as à chaque début de niveau dans ton inventaire, ce dernier se situe à gauche de l'écran, tu dois les placer avant de lever l'ancre du bateau.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/krabby.png"" alt="Krabby">
+            <p>Krabby : c'est un crabe, on ne sait pas d'où cette espèce vient, la légende raconte qu'il vient en réalité d'un autre univers où il existe plein de différentes créatures comme lui avec différentes capacités. Si tu croises son chemin, il va réussir avec la capacité de ses crochets de couper un trident.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/pieuvre.png"" alt="Pieuvre crocheteuse">
+            <p>Pieuvre crocheteuse : Description de la pieuvre crocheteuse.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/la_main.png"" alt="La main de l'eau">
+            <p>La main de l'eau : Description de la main de l'eau.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/guardian1.png"" alt="Guardian">
+            <p>Guardian : Description du Guardian.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/vague.png"" alt="Vague">
+            <p>Vague : Description de la vague.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/rock1.png"" alt="Rocher">
+            <p>Rocher : si tu passes dessus t'exploses en gros.</p>
+        </div>
+        <div class="obstacle">
+            <img src="../sprites/32x32/rock.png"" alt="Rocher1">
+            <p>Rocher : si tu passes dessus t'exploses en gros.</p>
+        </div>
+    </main>
+</body>
 </html>
