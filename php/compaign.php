ea674af821416e743a1148d80ee398c620c8533c
MartelFlavien <flavien.martel@student.junia.com>
2024-06-05 10:33:59 +0200

    KK

--- php/compaign.php
+++ php/compaign.php
@@ -8,52 +8,52 @@
 </head>
 <body>
 <img src="../img/campaign.png" alt="Campaign" class="logo">
-<a href="game.php" class="back-arrow"><img src="../img/bouton_retour.png" alt="Back"></a>
-    <div class="container">
-        <div class="arrow" id="left-arrow">&#9664;</div>
-        <div class="level-buttons" id="level-buttons">
-   
+<div class="back">
+    <a href="game.php" class="back-arrow">
+        <img src="../img/bouton_retour.png" alt="Back" class="link-image">
+    </a>
+</div>
+<div class="container">
+    <div class="arrow" id="left-arrow">&#9664;</div>
+    <div class="level-buttons" id="level-buttons"></div>
+    <div class="arrow" id="right-arrow">&#9654;</div>
 </div>
 
-        <div class="arrow" id="right-arrow">&#9654;</div>
-    </div>
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const leftArrow = document.getElementById('left-arrow');
+        const rightArrow = document.getElementById('right-arrow');
+        const levelButtons = document.getElementById('level-buttons');
 
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const leftArrow = document.getElementById('left-arrow');
-            const rightArrow = document.getElementById('right-arrow');
-            const levelButtons = document.getElementById('level-buttons');
+        let currentLevelSet = 1;
 
-            let currentLevelSet = 1;
-
-            leftArrow.addEventListener('click', () => {
-                if (currentLevelSet > 1) {
-                    currentLevelSet--;
-                    updateLevels();
-                }
-            });
-
-            rightArrow.addEventListener('click', () => {
-                if (currentLevelSet < 2) { 
-                    currentLevelSet++;
-                    updateLevels();
-                }
-            });
-
-            function updateLevels() {
-    const levels = [];
-    for (let i = 1; i <= 4; i++) {
-        const levelNumber = (currentLevelSet - 1) * 4 + i;
-        levels.push(`<a href='page_for_level_${levelNumber}.html' class='level-link'><img src='../img/level${levelNumber}.png' alt='Level ${levelNumber}' /></a>`);
-    }
-    levelButtons.innerHTML = levels.join('');
-    leftArrow.classList.toggle('disabled', currentLevelSet === 1);
-    rightArrow.classList.toggle('disabled', currentLevelSet === 2);
-}
-
-
-            updateLevels(); 
+        leftArrow.addEventListener('click', () => {
+            if (currentLevelSet > 1) {
+                currentLevelSet--;
+                updateLevels();
+            }
         });
-    </script>
+
+        rightArrow.addEventListener('click', () => {
+            if (currentLevelSet < 2) { 
+                currentLevelSet++;
+                updateLevels();
+            }
+        });
+
+        function updateLevels() {
+            const levels = [];
+            for (let i = 1; i <= 4; i++) {
+                const levelNumber = (currentLevelSet - 1) * 4 + i;
+                levels.push(`<a href='page_for_level_${levelNumber}.html' class='level-link'><img src='../img/level${levelNumber}.png' alt='Level ${levelNumber}' /></a>`);
+            }
+            levelButtons.innerHTML = levels.join('');
+            leftArrow.classList.toggle('disabled', currentLevelSet === 1);
+            rightArrow.classList.toggle('disabled', currentLevelSet === 2);
+        }
+
+        updateLevels(); 
+    });
+</script>
 </body>
 </html>
