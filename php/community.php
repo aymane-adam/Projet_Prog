8a9ea6580984ce1fb490ad19a900d50f5c9eef3b
Calliste-Lefebvre <calliste.lefebvre@student.junia.com>
2024-06-11 15:35:15 +0200

    s

--- php/community.php
+++ php/community.php
@@ -9,7 +9,7 @@
 <img src="../img/communitymode.png" alt="Community" class="logo">
 <a href="game.php" class="back-arrow"><img src="../img/bouton_retour.png" alt="Back"></a>
 <div class="search-container">
-    <input type="text" id="searchBox" placeholder="Search by ID or nickname...">
+    <input type="text" id="searchBox" placeholder="Search by ID...">
     <button onclick="searchLevel()">Search</button>
 </div>
 <div class="sort-container">
@@ -21,40 +21,26 @@
     <?php
         require("bdd.php");
 
+        
         $idFilter = isset($_GET['id']) ? intval($_GET['id']) : null;
-        $nameFilter = isset($_GET['nom_niveau']) ? $_GET['nom_niveau'] : null;
         $sortMethod = isset($_GET['sort']) ? $_GET['sort'] : null;
 
+      
         $query = "SELECT nom_niveau, createur, type_niveau, id_niveau FROM niveaux";
-        $conditions = [];
-
         if ($idFilter !== null) {
-            $conditions[] = "id_niveau = :id";
-        }
-        if ($nameFilter !== null) {
-            $conditions[] = "nom_niveau LIKE :nom_niveau";
+            $query .= " WHERE id_niveau = :id";
         }
 
-        if (!empty($conditions)) {
-            $query .= " WHERE " . implode(" AND ", $conditions);
-        }
-
-        if ($sortMethod !== null) {
-            if ($sortMethod == 'date') {
-                $query .= " ORDER BY date_creation DESC";
-            } elseif ($sortMethod == 'difficulty') {
-                $query .= " ORDER BY difficulty ASC";
-            }
+        if ($sortMethod === 'date') {
+            $query .= " ORDER BY date_creation DESC";
+        } elseif ($sortMethod === 'difficulty') {
+            $query .= " ORDER BY difficulte ASC";
         }
 
         $stmt = $conn->prepare($query);
 
         if ($idFilter !== null) {
             $stmt->bindParam(':id', $idFilter, PDO::PARAM_INT);
-        }
-        if ($nameFilter !== null) {
-            $likeNameFilter = "%".$nameFilter."%";
-            $stmt->bindParam(':nom_niveau', $likeNameFilter, PDO::PARAM_STR);
         }
 
         $stmt->execute();
@@ -69,21 +55,21 @@
                     <th>Select</th>
                 </tr>";
         foreach($resultat as $val){
-            if($val['type_niveau'] != 1){
-                echo "<tr>
-                        <td>".$val['nom_niveau']."</td>
-                        <td>".$val['createur']."</td>";
-                if($val['type_niveau'] == 2){
-                    echo "<td>Created by a user</td>";
-                }
-                if($val['type_niveau'] == 3){
-                    echo "<td>Created randomly</td>";
-                }
-                echo "<td>".$val['id_niveau']."</td>
-                      <td><a href='level.php?id=".$val['id_niveau']."'>Play</a></td>
-                    </tr>";
-            }
+            if($val['type_niveau']!= 1){
+            echo "<tr>
+                    <td>".$val['nom_niveau']."</td>
+                    <td>".$val['createur']."</td>";
+                    if($val['type_niveau'] == 2){
+                        echo "<td>Created by a user</td>";
+                    }
+                    if($val['type_niveau'] == 3){
+                        echo "<td>Created randomly</td>";
+                    }
+                    echo "<td>".$val['id_niveau']."</td>
+                        <td><a href='level.php?id=".$val['id_niveau']."'>Play</a></td>
+                </tr>";
         }
+    }
         echo "</table>";
     ?>
 </div>
@@ -93,12 +79,8 @@
         fetchLevels();
 
         window.searchLevel = function() {
-            const searchValue = document.getElementById('searchBox').value;
-            if (isNaN(searchValue)) {
-                fetchLevels(null, null, searchValue);
-            } else {
-                fetchLevels(searchValue);
-            }
+            const id = document.getElementById('searchBox').value;
+            fetchLevels(id);
         }
 
         window.sortLevels = function(method) {
@@ -106,10 +88,9 @@
         }
     });
 
-    function fetchLevels(searchId = null, sortMethod = null, searchName = null) {
+    function fetchLevels(searchId = null, sortMethod = null) {
         const params = new URLSearchParams();
         if (searchId) params.append('id', searchId);
-        if (searchName) params.append('nom_niveau', searchName);
         if (sortMethod) params.append('sort', sortMethod);
 
         fetch(window.location.pathname + '?' + params.toString())
