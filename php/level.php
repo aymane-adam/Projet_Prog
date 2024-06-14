bf3a631ea00c0da7b536a483dfb19d3e7443aed4
MartelFlavien <flavien.martel@student.junia.com>
2024-06-14 09:23:54 +0200

    BUG FLECHE REGLE

--- php/level.php
+++ php/level.php
@@ -154,216 +154,218 @@
     </div>
 
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const images = document.querySelectorAll(".image-container img");
-            const imageCount = {};
-            const maxImages = 2;
-            let matrice = <?php echo json_encode($matrice); ?>;
-            let directBoat = "<?php echo $directBoat; ?>";
-            let interval;
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".image-container img");
+    let imageCount = {
+        8: parseInt(document.getElementById('counter8').textContent),
+        9: parseInt(document.getElementById('counter9').textContent),
+        10: parseInt(document.getElementById('counter10').textContent),
+        11: parseInt(document.getElementById('counter11').textContent)
+    };
+    let matrice = <?php echo json_encode($matrice); ?>;
+    let directBoat = "<?php echo $directBoat; ?>";
+    let interval;
 
-            images.forEach(image => {
-                const value = image.getAttribute("data-value");
-                imageCount[value] = maxImages;
-                image.addEventListener("dragstart", function(event) {
+    images.forEach(image => {
+        image.addEventListener("dragstart", function(event) {
+            event.dataTransfer.setData("text/plain", event.target.id);
+        });
+    });
+
+    const dropBoxes = document.querySelectorAll(".drop-box");
+
+    dropBoxes.forEach(box => {
+        box.addEventListener("dragover", function(event) {
+            event.preventDefault();
+        });
+
+        box.addEventListener("drop", function(event) {
+            event.preventDefault();
+            const imageId = event.dataTransfer.getData("text/plain");
+            const value = parseInt(document.getElementById(imageId).getAttribute("data-value"));
+            const row = box.parentElement.getAttribute('data-row');
+            const col = box.parentElement.getAttribute('data-col');
+
+            if (box.getAttribute("data-value") === "0" && imageCount[value] > 0) {
+                const draggableElement = document.getElementById(imageId).cloneNode(true);
+                draggableElement.addEventListener("dragstart", function(event) {
                     event.dataTransfer.setData("text/plain", event.target.id);
                 });
-            });
-
-            const dropBoxes = document.querySelectorAll(".drop-box");
-
-            dropBoxes.forEach(box => {
-                box.addEventListener("dragover", function(event) {
-                    event.preventDefault();
-                });
-
-                box.addEventListener("drop", function(event) {
-                    event.preventDefault();
-                    const imageId = event.dataTransfer.getData("text/plain");
-                    const value = parseInt(document.getElementById(imageId).getAttribute("data-value"));
-                    const row = box.parentElement.getAttribute('data-row');
-                    const col = box.parentElement.getAttribute('data-col');
-
-                    if (box.getAttribute("data-value") === "0" && imageCount[value] > 0) {
-                        const draggableElement = document.getElementById(imageId).cloneNode(true);
-                        draggableElement.addEventListener("dragstart", function(event) {
-                            event.dataTransfer.setData("text/plain", event.target.id);
-                        });
-                        box.innerHTML = ''; // Clear any existing content
-                        box.appendChild(draggableElement);
-                        box.setAttribute("data-value", value);
-                        imageCount[value]--;
-                        matrice[row][col] = value;
-                        updateMatriceDisplay();
-                        updateCounter(value);
-                    }
-                });
-            });
-
-            const trashContainer = document.querySelector(".trash-container");
-
-            trashContainer.addEventListener("dragover", function(event) {
-                event.preventDefault();
-            });
-
-            trashContainer.addEventListener("drop", function(event) {
-                event.preventDefault();
-                const imageId = event.dataTransfer.getData("text/plain");
-                const draggableElement = document.getElementById(imageId);
-                const value = parseInt(draggableElement.getAttribute("data-value"));
-                const dropBox = draggableElement.parentElement;
-                const row = dropBox.parentElement.getAttribute('data-row');
-                const col = dropBox.parentElement.getAttribute('data-col');
-
-                if (dropBox.classList.contains("drop-box")) {
-                    dropBox.innerHTML = ''; // Remove the image from the drop box
-                    dropBox.setAttribute("data-value", "0");
-                    imageCount[value]++;
-                    matrice[row][col] = 0;
-                    updateMatriceDisplay();
-                    updateCounter(value);
-                }
-            });
-
-            function updateCounter(value) {
-                const counter = document.getElementById('counter' + value);
-                counter.textContent = imageCount[value];
-            }
-
-            function updateMatriceDisplay() {
-                const matriceDisplay = document.getElementById('matrice-display');
-                matriceDisplay.innerHTML = '';
-                matrice.forEach(ligne => {
-                    matriceDisplay.innerHTML += ligne.join(" ") + "\n";
-                });
-            }
-
-            function moveBoat() {
-                let boatPosition = findBoatPosition();
-                if (!boatPosition) {
-                    return;
-                }
-
-                let [row, col] = boatPosition;
-                matrice[row][col] = 0;
-                let nextRow = row, nextCol = col;
-
-                switch (directBoat) {
-                    case "nord":
-                        nextRow--;
-                        break;
-                    case "sud":
-                        nextRow++;
-                        break;
-                    case "ouest":
-                        nextCol--;
-                        break;
-                    case "est":
-                        nextCol++;
-                        break;
-                }
-
-                if (nextRow >= 0 && nextRow < matrice.length && nextCol >= 0 && nextCol < matrice[0].length) {
-                    let nextCell = matrice[nextRow][nextCol];
-                    if (nextCell === 0) {
-                        matrice[nextRow][nextCol] = 1;
-                    } else if (nextCell === 8) {
-                        directBoat = "nord";
-                        matrice[nextRow][nextCol] = 1;
-                    } else if (nextCell === 9) {
-                        directBoat = "sud";
-                        matrice[nextRow][nextCol] = 1;
-                    } else if (nextCell === 10) {
-                        directBoat = "ouest";
-                        matrice[nextRow][nextCol] = 1;
-                    } else if (nextCell === 11) {
-                        directBoat = "est";
-                        matrice[nextRow][nextCol] = 1;
-                    } else if (nextCell === 12) {
-                        clearInterval(interval);                 
-                        document.getElementById('message').textContent = "Partie gagnée!";
-                        document.getElementById('message').classList.remove('hidden');
-                        document.getElementById('recommencer').classList.remove('hidden');
-                        document.getElementById('suivant').classList.remove('hidden');
-                    } else {
-                        matrice[nextRow][nextCol] = 13; // Code pour "boom"
-                        clearInterval(interval);
-                        document.getElementById('message').textContent = "Game Over";
-                        document.getElementById('message').classList.remove('hidden');
-                        document.getElementById('commencer').classList.add('hidden');
-                        document.getElementById('retry').classList.remove('hidden');
-                    }
-                } else {
-                    clearInterval(interval);
-                    document.getElementById('message').textContent = "Game Over";
-                    document.getElementById('message').classList.remove('hidden');
-                    document.getElementById('commencer').classList.add('hidden');
-                    document.getElementById('retry').classList.remove('hidden');
-                }
-
-                updateGridDisplay();
+                box.innerHTML = ''; // Clear any existing content
+                box.appendChild(draggableElement);
+                box.setAttribute("data-value", value);
+                imageCount[value]--;
+                matrice[row][col] = value;
                 updateMatriceDisplay();
-                document.getElementById('directionBoat').textContent = directBoat;
+                updateCounter(value);
             }
-
-            function findBoatPosition() {
-                for (let i = 0; i < matrice.length; i++) {
-                    for (let j = 0; j < matrice[i].length; j++) {
-                        if (matrice[i][j] === 1) {
-                            return [i, j];
-                        }
-                    }
-                }
-                return null;
-            }
-
-            function updateGridDisplay() {
-                const grid = document.getElementById('grid');
-                grid.innerHTML = '';
-                matrice.forEach((ligne, ligneIdx) => {
-                    let tr = document.createElement('tr');
-                    ligne.forEach((cellule, celluleIdx) => {
-                        let td = document.createElement('td');
-                        td.setAttribute('data-row', ligneIdx);
-                        td.setAttribute('data-col', celluleIdx);
-                        if (cellule !== 0) {
-                            let img = document.createElement('img');
-                            if (cellule === 13) {
-                                img.src = "../pixel_art_projet/32x32/boom.png";
-                            } else {
-                                img.src = "../pixel_art_projet/32x32/" + <?php echo json_encode($images); ?>[cellule];
-                            }
-                            img.classList.add('drop-box');
-                            img.setAttribute('data-value', cellule);
-                            td.appendChild(img);
-                        } else {
-                            let div = document.createElement('div');
-                            div.classList.add('drop-box');
-                            div.setAttribute('data-value', '0');
-                            td.appendChild(div);
-                        }
-                        tr.appendChild(td);
-                    });
-                    grid.appendChild(tr);
-                });
-            }
-
-            document.getElementById('commencer').addEventListener('click', function() {
-                interval = setInterval(moveBoat, 1000); // Utiliser directBoat initialisé dans PHP
-            });
-
-            document.getElementById('retry').addEventListener('click', function() {
-                location.reload();
-            });
-
-            document.getElementById('recommencer').addEventListener('click', function() {
-                location.reload();
-            });
-
-            document.getElementById('suivant').addEventListener('click', function() {
-                window.location.href = "level.php?id=" + (<?php echo $levelNumber; ?> + 1);
-            });
         });
+    });
+
+    const trashContainer = document.querySelector(".trash-container");
+
+    trashContainer.addEventListener("dragover", function(event) {
+        event.preventDefault();
+    });
+
+    trashContainer.addEventListener("drop", function(event) {
+        event.preventDefault();
+        const imageId = event.dataTransfer.getData("text/plain");
+        const draggableElement = document.getElementById(imageId);
+        const value = parseInt(draggableElement.getAttribute("data-value"));
+        const dropBox = draggableElement.parentElement;
+        const row = dropBox.parentElement.getAttribute('data-row');
+        const col = dropBox.parentElement.getAttribute('data-col');
+
+        if (dropBox.classList.contains("drop-box")) {
+            dropBox.innerHTML = ''; // Remove the image from the drop box
+            dropBox.setAttribute("data-value", "0");
+            imageCount[value]++;
+            matrice[row][col] = 0;
+            updateMatriceDisplay();
+            updateCounter(value);
+        }
+    });
+
+    function updateCounter(value) {
+        const counter = document.getElementById('counter' + value);
+        counter.textContent = imageCount[value];
+    }
+
+    function updateMatriceDisplay() {
+        const matriceDisplay = document.getElementById('matrice-display');
+        matriceDisplay.innerHTML = '';
+        matrice.forEach(ligne => {
+            matriceDisplay.innerHTML += ligne.join(" ") + "\n";
+        });
+    }
+
+    function moveBoat() {
+        let boatPosition = findBoatPosition();
+        if (!boatPosition) {
+            return;
+        }
+
+        let [row, col] = boatPosition;
+        matrice[row][col] = 0;
+        let nextRow = row, nextCol = col;
+
+        switch (directBoat) {
+            case "nord":
+                nextRow--;
+                break;
+            case "sud":
+                nextRow++;
+                break;
+            case "ouest":
+                nextCol--;
+                break;
+            case "est":
+                nextCol++;
+                break;
+        }
+
+        if (nextRow >= 0 && nextRow < matrice.length && nextCol >= 0 && nextCol < matrice[0].length) {
+            let nextCell = matrice[nextRow][nextCol];
+            if (nextCell === 0) {
+                matrice[nextRow][nextCol] = 1;
+            } else if (nextCell === 8) {
+                directBoat = "nord";
+                matrice[nextRow][nextCol] = 1;
+            } else if (nextCell === 9) {
+                directBoat = "sud";
+                matrice[nextRow][nextCol] = 1;
+            } else if (nextCell === 10) {
+                directBoat = "ouest";
+                matrice[nextRow][nextCol] = 1;
+            } else if (nextCell === 11) {
+                directBoat = "est";
+                matrice[nextRow][nextCol] = 1;
+            } else if (nextCell === 12) {
+                clearInterval(interval);                 
+                document.getElementById('message').textContent = "Partie gagnée!";
+                document.getElementById('message').classList.remove('hidden');
+                document.getElementById('recommencer').classList.remove('hidden');
+                document.getElementById('suivant').classList.remove('hidden');
+            } else {
+                matrice[nextRow][nextCol] = 13; // Code pour "boom"
+                clearInterval(interval);
+                document.getElementById('message').textContent = "Game Over";
+                document.getElementById('message').classList.remove('hidden');
+                document.getElementById('commencer').classList.add('hidden');
+                document.getElementById('retry').classList.remove('hidden');
+            }
+        } else {
+            clearInterval(interval);
+            document.getElementById('message').textContent = "Game Over";
+            document.getElementById('message').classList.remove('hidden');
+            document.getElementById('commencer').classList.add('hidden');
+            document.getElementById('retry').classList.remove('hidden');
+        }
+
+        updateGridDisplay();
+        updateMatriceDisplay();
+        document.getElementById('directionBoat').textContent = directBoat;
+    }
+
+    function findBoatPosition() {
+        for (let i = 0; i < matrice.length; i++) {
+            for (let j = 0; j < matrice[i].length; j++) {
+                if (matrice[i][j] === 1) {
+                    return [i, j];
+                }
+            }
+        }
+        return null;
+    }
+
+    function updateGridDisplay() {
+        const grid = document.getElementById('grid');
+        grid.innerHTML = '';
+        matrice.forEach((ligne, ligneIdx) => {
+            let tr = document.createElement('tr');
+            ligne.forEach((cellule, celluleIdx) => {
+                let td = document.createElement('td');
+                td.setAttribute('data-row', ligneIdx);
+                td.setAttribute('data-col', celluleIdx);
+                if (cellule !== 0) {
+                    let img = document.createElement('img');
+                    if (cellule === 13) {
+                        img.src = "../pixel_art_projet/32x32/boom.png";
+                    } else {
+                        img.src = "../pixel_art_projet/32x32/" + <?php echo json_encode($images); ?>[cellule];
+                    }
+                    img.classList.add('drop-box');
+                    img.setAttribute('data-value', cellule);
+                    td.appendChild(img);
+                } else {
+                    let div = document.createElement('div');
+                    div.classList.add('drop-box');
+                    div.setAttribute('data-value', '0');
+                    td.appendChild(div);
+                }
+                tr.appendChild(td);
+            });
+            grid.appendChild(tr);
+        });
+    }
+
+    document.getElementById('commencer').addEventListener('click', function() {
+        interval = setInterval(moveBoat, 1000); // Utiliser directBoat initialisé dans PHP
+    });
+
+    document.getElementById('retry').addEventListener('click', function() {
+        location.reload();
+    });
+
+    document.getElementById('recommencer').addEventListener('click', function() {
+        location.reload();
+    });
+
+    document.getElementById('suivant').addEventListener('click', function() {
+        window.location.href = "level.php?id=" + (<?php echo $levelNumber; ?> + 1);
+    });
+});
     </script>
     <?php     
 
